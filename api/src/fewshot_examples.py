7ec7b306a9e9cfd5096157944af620670030debc
Vetri190591 <139077431+Vetri190591@users.noreply.github.com>
2023-07-20 17:23:02 +0100

    fewshot ex added

--- api/src/fewshot_examples.py
+++ api/src/fewshot_examples.py
@@ -1,5 +1,4 @@
-def get_fewshot_examples(openai_api_key):
-    return f"""
+ return f"""
 #How is Emil Eifrem connected to Michael Hunger?
 MATCH (p1:Person {{name:"Emil Eifrem"}}), (p2:Person {{name:"Michael Hunger"}})
 MATCH p=shortestPath((p1)-[*]-(p2))
@@ -23,9 +22,20 @@ WITH c, gds.similarity.cosine(c.embedding, embedding) AS score
 ORDER BY score DESC LIMIT 3
 RETURN c.text, score
 #What are the relationships associated with the batch node?
-MATCH (n) MATCH ()-[r]->() RETURN n, r
+MATCH (Batch) MATCH ()-[r]->() RETURN Batch, r
 #What are the properties of my node?
 Match (n) return properties(n), ID(n)
+#What are the properties of Batch?
+Match (Batch) return properties(Batch)
+#What are the sites nucala is manufactured?
+match(m:Batch)-[made_of]->(n:Material) where n.`material desc` contains 'nucala' return m.sitecode
+#What are the various sites nucala is manufactured?
+CALL apoc.ml.openai.embedding(["Which sites nucala is manufactured?"], "{openai_api_key}") YIELD embedding
+MATCH (n:Material {`material desc`:"nucala"})<-[:made_of]-()-[:HAS_CHUNK]->(c)
+WITH distinct c, embedding
+WITH c, gds.similarity.cosine(c.embedding, embedding) AS score
+ORDER BY score DESC LIMIT 3
+RETURN c.text, score
 When searching for specific information in the text chunks, never use the CONTAINS clause, but always use the apoc.ml.openai.embedding
 and gds.similarity.cosine functions as shown in the examples.
 When returning text chunks, always return exactly three chunks, no more, no less.
